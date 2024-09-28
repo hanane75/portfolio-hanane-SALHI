e1fa58acacee864868db59d813fe59b87ea1886d
hanane75 <benkada.hanane@gmail.com>
2024-09-28 18:03:13 +0200

    creat app + fichiers

---
+++ portfolio-salhi-hanane/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
